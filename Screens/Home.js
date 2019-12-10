19a0ac929a1250c4307161a02db7d192fb137163
Conor Scott <scot0359@algonquinlive.com>
2019-12-09 23:32:22 -0500

    fetching data

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import { Container } from 'native-base';
+import { Container, Button } from 'native-base';
 import { StyleSheet, Text, View } from 'react-native';
-import axios from 'axios';
+import { AppLoading } from 'expo'
 
 export default class Home extends Component {
 
@@ -27,15 +27,35 @@ export default class Home extends Component {
         let lat = this.state.where.lat
         let lng = this.state.where.lng
 
+        // https://api.yelp.com/v3/businesses/search?latitude=${lat}&longitude=${lng}
+        // https://api.yelp.com/v3/businesses/search?latitude=37.785834&longitude=-122.406417
+
         fetch(`https://api.yelp.com/v3/businesses/search?latitude=${lat}&longitude=${lng}`, {
+            method: 'GET',
             headers: {
                 'Authorization': 'Bearer YDjD-LAWi6jZf1ImD7yjwqLcrDfD1c9vwJNF7tXplA7hT5BzsBlbJiTg0Y3wcCr4Y-0f76ADuroc5-RBtHxEE2i4qIyla4FHmwq3T5-OkQn95hPhVOo6w7fVnf_uXXYx'
             }
         })
         .then(response => response.json())
         .then(data => {
-            this.setState({ yelpList: data.businesses})
-        })      
+            this.setState({ yelpList: JSON.stringify(data.businesses)})
+        })    
+        
+        // let url = `https://api.yelp.com/v3/businesses/search?latitude=${lat}&longitude=${lng}`
+        // let h = new Headers();
+        // h.append('Authorization', 'Bearer YDjD-LAWi6jZf1ImD7yjwqLcrDfD1c9vwJNF7tXplA7hT5BzsBlbJiTg0Y3wcCr4Y-0f76ADuroc5-RBtHxEE2i4qIyla4FHmwq3T5-OkQn95hPhVOo6w7fVnf_uXXYx')
+
+        // let req = new Request(url, {
+        //     headers: h,
+        //     method: 'GET'
+        // })
+
+        // fetch(req)
+        // .then(response => response.json())
+        // .then(data => {
+        //     this.setState({yelpList: JSON.stringify(data)})
+        //     console.log('LIST', this.state.yelpList)
+        // })
 
     
     }
@@ -56,12 +76,13 @@ export default class Home extends Component {
         navigator.geolocation.getCurrentPosition
             (this.geoSuccess, this.geoFailure, geoOptions)
 
-            this.loadRestaurants()
+            // this.loadRestaurants()
 
             console.log("list:", this.state.yelpList)
     }
 
     render() {
+
         return (
             <Container>
                 <View style={styles.container}>
@@ -77,6 +98,8 @@ export default class Home extends Component {
                             Longitude: ${this.state.where.lng}`
                         }</Text>
                     )}
+
+                    <Button onPress={this.loadRestaurants}><Text>PRESSME</Text></Button>
 
                     <Text>{`Restaurants: ${this.state.yelpList}`}</Text>
                 </View>
