d57554e1049136818f2ac70b1659097616820614
Conor Scott <scot0359@algonquinlive.com>
2019-12-12 10:18:46 -0500

    updated styling

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Home from './Screens/Home'
-import YelpDetails from './Screens/YelpDetails'
+import Home from './screens/Home'
+import YelpDetails from './screens/YelpDetails'
 import { createStackNavigator } from 'react-navigation-stack'
 import { createAppContainer } from 'react-navigation'
 
@@ -16,19 +16,9 @@ const AppNavigator = createStackNavigator({
 }, {
   initialRouteName: 'Home',
   defaultNavigationOptions: {
-    headerStyle: { backgroundColor: 'hsl(220, 100%, 80%)' }
+    headerStyle: { backgroundColor: '#CD1305', fontColor: '#FFFFFF' },
+    headerTintColor: '#FFFFFF'
   }
 })
 
 export default createAppContainer(AppNavigator)
-
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
